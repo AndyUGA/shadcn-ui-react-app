43999317b344ad0f616e290e22143c0fdb2f9492
Andy Truong <andytruongcs@gmail.com>
2024-01-24 18:02:40 -0500

    Installed all necessary files to start using Shadcn UI components

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <Button>
+        Click Me
+      </Button>
+    </>
+  )
+}
+
+export default App
