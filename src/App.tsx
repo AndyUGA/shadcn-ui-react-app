acdc97571e0009ede78fe5d69d85b4e714522c3e
Andy Truong <andytruongcs@gmail.com>
2024-02-22 21:38:17 -0500

    Add code for pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,97 @@
 
 import './App.css'
-import { Button } from './components/ui/button'
+import axios from 'axios';
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
+import { useEffect, useState } from 'react'
+
+interface Post {
+  id: number;
+  body: string;
+}
 
 function App() {
+  const rowsPerPage = 10;
+  const [data, setData] = useState<Post[]>([]);
+  const [startIndex, setStartIndex] = useState(0);
+  const [endIndex, setEndIndex] = useState(rowsPerPage);
+
+
+  const getData = async () => {
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      const data = response.data;
+      console.log(38, data);
+      setData(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+  useEffect(() => {
+    getData();
+  }, [])
 
   return (
     <>
-      <Button>
-        Click Me
-      </Button>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Id</TableHead>
+            <TableHead>Body</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {data.slice(startIndex, endIndex).map((item) => {
+            return <>
+              <TableRow>
+                <TableCell className="text-left">{item.id}</TableCell>
+                <TableCell className="text-left">{item.body}</TableCell>
+              </TableRow>
+            </>
+          })}
+
+        </TableBody>
+      </Table>
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious
+              className={
+                startIndex === 0 ? "pointer-events-none opacity-50" : undefined
+              }
+              onClick={() => {
+                setStartIndex(startIndex - rowsPerPage);
+                setEndIndex(endIndex - rowsPerPage);
+              }} />
+          </PaginationItem>
+
+          <PaginationItem>
+            <PaginationNext
+              className={
+                endIndex === 100 ? "pointer-events-none opacity-50" : undefined
+              }
+              onClick={() => {
+                setStartIndex(startIndex + rowsPerPage); //10
+                setEndIndex(endIndex + rowsPerPage); //10 + 10 = 20
+              }} />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+
     </>
   )
 }
